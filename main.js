5fef721c6e6168a07735c3984feed027982c3acd
Ali <alirabbani54@gmail.com>
2023-08-22 12:17:24 +0500

    music web app init repo

---
+++ main.js
@@ -0,0 +1,16 @@
+const carousel = document.querySelector(".carousel");
+
+let isDragging = false;
+
+const dragStart = () =>{
+    isDragging = true;
+
+}
+
+const dragging = (e) => {
+    if(!isDragging) return;
+    carousel.scrollLeft = e.pageX;
+} 
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
