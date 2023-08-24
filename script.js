784a6fc5ea359ecca1ff0200608400c1a4e2df02
Ali <alirabbani54@gmail.com>
2023-08-24 09:48:51 +0500

    slider progress update

--- script.js
+++ script.js
@@ -2,52 +2,61 @@ console.log("Hello Listeners!");
 
 //Intialized Variables
 let songIndex = 0;
-let audioElement = new Audio("./assets/audio/Cartoon-On&On.mp3");
+let audioElement = new Audio("./assets/audio/Cartoon - On & On.mp3");
 let playButton = document.getElementById("play");
 let pauseButton = document.getElementById("pause");
 let playingGif = document.getElementById("playing-gif");
 let audioSeeker = document.getElementById("seeker");
 
+//Populate Songs LIst
 // Songs List
 let songs = [
   {
-    songName: "Cartoon-On&On",
-    filePath: "./assets/audio/Cartoon-On&On.mp3",
+    songName: "On & On",
+    artistName: "Cartoon",
+    filePath: "./assets/audio/Cartoon - On & On.mp3",
     coverPath: "./assets/covers/cartoon-on&on.jpg",
   },
   {
-    songName: "WhyWelose",
-    filePath: "./assets/audio/Cartoon-On&On.mp3",
+    songName: "Why We lose",
+    artistName: "Cartoon",
+    filePath: "./assets/audio/Cartoon - Why We Lose.mp3",
     coverPath: "./assets/covers/cartoon-on&on.jpg",
   },
   {
     songName: "invincible",
-    filePath: "./assets/audio/Cartoon-On&On.mp3",
+    artistName: "DEAFKEV",
+    filePath: "./assets/audio/DEAFKEV - Invincible.mp3",
     coverPath: "./assets/covers/cartoon-on&on.jpg",
   },
   {
-    songName: "On$On",
-    filePath: "./assets/audio/Cartoon-On&On.mp3",
+    songName: "Blank",
+    artistName: "Disfigure",
+    filePath: "./assets/audio/Disfigure - Blank.mp3",
     coverPath: "./assets/covers/cartoon-on&on.jpg",
   },
   {
-    songName: "On$On",
-    filePath: "./assets/audio/Cartoon-On&On.mp3",
+    songName: "Sky High",
+    artistName: "Elektronomia",
+    filePath: "./assets/audio/Elektronomia - Sky High.mp3",
     coverPath: "./assets/covers/cartoon-on&on.jpg",
   },
   {
-    songName: "On$On",
-    filePath: "./assets/audio/Cartoon-On&On.mp3",
+    songName: "Heroes Tonight",
+    artistName: "Janji",
+    filePath: "./assets/audio/Janji - Heroes Tonight.mp3",
     coverPath: "./assets/covers/cartoon-on&on.jpg",
   },
   {
-    songName: "On$On",
-    filePath: "./assets/audio/Cartoon-On&On.mp3",
+    songName: "Cradles",
+    artistName: "Sub Urban",
+    filePath: "./assets/audio/Sub Urban - Cradles.mp3",
     coverPath: "./assets/covers/cartoon-on&on.jpg",
   },
   {
-    songName: "On$On",
-    filePath: "./assets/audio/Cartoon-On&On.mp3",
+    songName: "Mortals",
+    artistName: "Warriyo",
+    filePath: "./assets/audio/Warriyo - Mortals.mp3",
     coverPath: "./assets/covers/cartoon-on&on.jpg",
   },
 ];
@@ -65,11 +74,21 @@ playButton.addEventListener("click", (e) => {
     playButton.src = "./svg/play.png";
     audioElement.pause();
     playingGif.style.opacity = 0;
-
   }
 });
 
 // Listen to timeupdate
-audioSeeker.addEventListener("timeupdate", (e) => {
+audioElement.addEventListener("timeupdate", (e) => {
   console.log("timeupdate");
 });
+
+
+/* Slider Progress updater */
+
+//Seeker Progress
+audioSeeker.addEventListener('input', (e) => {
+    let widthProgress = parseInt((audioSeeker.value * 100)/100);
+
+    document.querySelector(".slider-progress").style.width = `${widthProgress}%`;
+
+})
